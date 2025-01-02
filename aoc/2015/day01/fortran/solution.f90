79c2219d94969c8fd141c7d9370db718fc8d138d
Fred Jones <fredjones42@proton.me>
2025-01-02 10:18:20 -0700

    Solve 2015 01 in Fortran

--- aoc/2015/day01/fortran/solution.f90
+++ aoc/2015/day01/fortran/solution.f90
@@ -1,7 +1,8 @@
 ! Advent of Code 2015 Day 01 - Fortran Solution Stub
 program day01
     implicit none
-    integer :: ios, lu
+    integer :: ios, lu, i, n
+    logical :: first
     character(len=16384) :: line
 
     open(newunit=lu, file="../input.txt", status="old", iostat=ios)
@@ -13,9 +14,25 @@ program day01
     do
         read(lu, '(a)', iostat=ios) line
         if (ios /= 0) exit
-        print *, trim(line)
     end do
     close(lu)
 
-    print *, "No Fortran solution has been written yet"
+    n = 0
+    first = .false.
+    do i = 1, len_trim(line)
+        if (line(i:i) == '(') then
+            n = n + 1
+        else if (line(i:i) == ')') then
+            n = n - 1
+        end if
+        if (.not.first) then
+            if (n == -1) then
+                print *, 'Santa enters basement at', i
+                first = .true.
+            end if
+        end if
+    end do
+
+    print *, 'Santa goes to floor', n
+
 end program day01

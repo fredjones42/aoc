9236aaab7776f2f9eb308484c32b7c05c8be7b4f
Fred Jones <fredjones42@proton.me>
2025-01-02 09:58:13 -0700

    Solve 2015 01 in Python

--- aoc/2015/day01/python/solution.py
+++ aoc/2015/day01/python/solution.py
@@ -1,12 +1,28 @@
 # Advent of Code 2015 Day 01 - Python Solution Stub
 
+
 def main():
     try:
-        with open('../input.txt', 'r') as f:
+        with open("../input.txt", "r") as f:
             data = f.read()
-        print("No Python solution has been written yet")
+        print("Santa goes to floor", data.count("(") - data.count(")"))
+        print("Santa enters basement at", pos_extra_rparen(data))
     except FileNotFoundError:
         print("Input file not found")
+
+
+def pos_extra_rparen(data):
+    n = 0
+    for i, c in enumerate(data):
+        if c == "(":
+            n += 1
+        elif c == ")":
+            n -= 1
+        else:
+            raise Exception("unexpected char")
+        if n == -1:
+            return i + 1
+    return 0
 
 
 if __name__ == "__main__":

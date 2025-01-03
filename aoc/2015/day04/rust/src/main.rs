05eb9f402d303e1c758d3cf4e33d9be77e452c7f
Fred Jones <fredjones42@proton.me>
2025-01-02 19:38:31 -0700

    Solve 2015 04 in Rust

--- aoc/2015/day04/rust/src/main.rs
+++ aoc/2015/day04/rust/src/main.rs
@@ -1,3 +1,44 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("../input.txt").expect("Input file not found");
+
+    let answer = find_answer_5(&input);
+    println!("The answer to Part One is: {}", answer);
+
+    let answer = find_answer_6(&input);
+    println!("The answer to Part Two is: {}", answer);
+}
+
+fn find_answer_5(secret_key: &str) -> i32 {
+    let mut n = 0;
+    loop {
+        let s = format!("{}{}", secret_key, n);
+        let digest = md5::compute(s.as_bytes());
+        if format!("{:x}", digest)[..5] == *"00000" {
+            return n;
+        }
+        n += 1;
+    }
+}
+
+fn find_answer_6(secret_key: &str) -> i32 {
+    let mut n = 0;
+    loop {
+        let s = format!("{}{}", secret_key, n);
+        let digest = md5::compute(s.as_bytes());
+        if format!("{:x}", digest)[..6] == *"000000" {
+            return n;
+        }
+        n += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        assert_eq!(find_answer_5("abcdef"), 609043);
+        assert_eq!(find_answer_5("pqrstuv"), 1048970);
+    }
 }

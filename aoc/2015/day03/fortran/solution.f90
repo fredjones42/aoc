b4e9af627ad227e91cb3ae90a8e1802e59b51991
Fred Jones <fredjones42@proton.me>
2025-01-02 19:07:48 -0700

    Solve 2015 03 in Fortran

--- aoc/2015/day03/fortran/solution.f90
+++ aoc/2015/day03/fortran/solution.f90
@@ -1,7 +1,8 @@
 ! Advent of Code 2015 Day 03 - Fortran Solution Stub
 program day03
     implicit none
-    integer :: ios, lu
+    integer :: i, ios, ir, j, jr, k, lu, n, sol1, sol2
+    integer, allocatable :: grid(:, :)
     character(len=16384) :: line
 
     open(newunit=lu, file="../input.txt", status="old", iostat=ios)
@@ -13,9 +14,73 @@ program day03
     do
         read(lu, '(a)', iostat=ios) line
         if (ios /= 0) exit
-        print *, trim(line)
+        n = len_trim(line)
     end do
     close(lu)
 
-    print *, "No Fortran solution has been written yet"
+    allocate(grid(-n:n, -n:n))
+    grid = 0
+    i = 0
+    j = 0
+    grid(i, j) = 1
+    do k = 1, n
+        if (line(k:k) == '^') then
+            j = j + 1
+        else if (line(k:k) == 'v') then
+            j = j - 1
+        else if (line(k:k) == '>') then
+            i = i + 1
+        else if (line(k:k) == '<') then
+            i = i - 1
+        end if
+        grid(i, j) = grid(i, j) + 1
+    end do
+    sol1 = 0
+    do j = -n, n
+        do i = -n, n
+            if (grid(i, j) > 0) sol1 = sol1 + 1
+        end do
+    end do
+    grid = 0
+    i = 0
+    j = 0
+    ir = 0
+    jr = 0
+    grid(i, j) = 2
+    do k = 1, n
+        if (mod(k, 2) == 1) then
+            if (line(k:k) == '^') then
+                j = j + 1
+            else if (line(k:k) == 'v') then
+                j = j - 1
+            else if (line(k:k) == '>') then
+                i = i + 1
+            else if (line(k:k) == '<') then
+                i = i - 1
+            end if
+            grid(i, j) = grid(i, j) + 1
+        else
+            if (line(k:k) == '^') then
+                jr = jr + 1
+            else if (line(k:k) == 'v') then
+                jr = jr - 1
+            else if (line(k:k) == '>') then
+                ir = ir + 1
+            else if (line(k:k) == '<') then
+                ir = ir - 1
+            end if
+            grid(ir, jr) = grid(ir, jr) + 1
+        end if
+    end do
+    sol2 = 0
+    do j = -n, n
+        do i = -n, n
+            if (grid(i, j) > 0) sol2 = sol2 + 1
+        end do
+    end do
+    deallocate(grid)
+
+    print *, sol1, "houses received at least one present."
+    print *, "With Robo-Santa..."
+    print *, sol2, "houses received at least one present."
 end program day03
